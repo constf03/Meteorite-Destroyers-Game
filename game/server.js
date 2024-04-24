d8512d9411907bd57b5b7e977ac515f0d6bc928e
AC9669 <ac9669@student.jamk.fi>
2024-04-24 12:58:25 +0300

    use process env conn string

--- game/server.js
+++ game/server.js
@@ -1,13 +1,16 @@
-const express = require('express') 
-const cors = require('cors')
-const app = express()
-const port = 3000
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
 
 // cors - allow connection from different domains and ports
-app.use(cors())
+app.use(cors());
 
 // convert json string to json object (from request)
-app.use(express.json())
+app.use(express.json());
 
 app.use(express.static('public'));
 
@@ -17,22 +20,22 @@ app.get('/', function (req, res) {
 
 // app listen port 3000
 app.listen(port, () => {
-  console.log(`Meteorite Destroyers listening on http://localhost:${port}`)
-})
+  console.log(`Meteorite Destroyers listening on http://localhost:${port}`);
+});
 
 // MongoDB
-const mongoose = require('mongoose')
-const mongoDB = 'mongodb+srv://ac9669:salasana@democluster2.vsyignr.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+const mongoose = require('mongoose');
+const mongoDB = process.env.URI
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
+db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log("MongoDB connected")
-})
+  console.log("MongoDB connected");
+});
 
 // route variables
-const highscoreRoute = require("./routes/highscore")
+const highscoreRoute = require("./routes/highscore");
 
 // routes
-app.use("/highscores", highscoreRoute)
+app.use("/highscores", highscoreRoute);
