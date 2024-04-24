82a8a06fbbcf3b373283d2af821ea11d9c570172
AC9669 <ac9669@student.jamk.fi>
2024-04-24 18:40:10 +0300

    update

--- game/public/leaderboard.js
+++ game/public/leaderboard.js
@@ -16,10 +16,10 @@ function createHighscoreListItem(highscore) {
     let li_attr = document.createAttribute('id');
     li_attr.value= highscore._id;
     li.setAttributeNode(li_attr);
-    let number = document.createTextNode(highscore.highscore)
-    li.appendChild(number)
+    let number = document.createTextNode(highscore.highscore);
+    li.appendChild(number);
 
-    return li
+    return li;
 }
 
 function showHighscores(highscores) {
@@ -36,5 +36,39 @@ function showHighscores(highscores) {
         });
         infoText.innerHTML = '';
     }
+    sortHighscores();
+}
 
+function sortHighscores() {
+    var list, i, switching, b, shouldSwitch;
+    list = document.getElementById("highscoreList");
+    switching = true;
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // start by saying: no switching is done:
+      switching = false;
+      b = list.getElementsByTagName("LI");
+      // Loop through all list-items:
+      for (i = 0; i < (b.length - 1); i++) {
+        // start by saying there should be no switching:
+        shouldSwitch = false;
+        /* check if the next item should
+        switch place with the current item: */
+        
+        if (Number(b[i].innerHTML) < Number(b[i + 1].innerHTML)) {
+          /* if next item is numerically
+          lower than current item, mark as a switch
+          and break the loop: */
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark the switch as done: */
+        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        switching = true;
+      }
+    }
 }
